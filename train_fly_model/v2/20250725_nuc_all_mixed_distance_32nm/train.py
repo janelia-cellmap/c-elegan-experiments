4619fa2d937d1b7934b6182c672b7cbeed63a111
Marwan Zouinkhi <zouinkhi.marwan@gmail.com>
2025-08-07 13:13:30 -0400

    clean

--- train_fly_model/v2/20250725_nuc_all_mixed_distance_32nm/train.py
+++ train_fly_model/v2/20250725_nuc_all_mixed_distance_32nm/train.py
@@ -1,67 +1,48 @@
-#%%
+import os
+import glob
 import yaml
+import torch
+import numpy as np
+import logging
+import warnings
 
+from fly_organelles.run import run, set_weights
+from fly_organelles.model import StandardUnet
+
+# Suppress only UserWarning and FutureWarning
+warnings.filterwarnings("ignore", category=UserWarning)
+warnings.filterwarnings("ignore", category=FutureWarning)
+
+# Load config
 with open("config.yaml", "r") as f:
     config = yaml.safe_load(f)
 
 labels = config["labels"]
 yaml_file = config["paths"]["yaml_file"]
 log_dir = config["paths"]["log_dir"]
-voxel_size = tuple([config["voxel_size"] for _ in range(3)])
-
+voxel_size = tuple([config["voxel_size"]] * 3)
 l_rate = 0.5e-5
 batch_size = 14
+weights_labels = [1 for _ in labels]
+iterations = 1_000_000
 
-
-weights_labes = [1 for _ in labels]
-
-
-import warnings
-
-# Suppress only UserWarning and FutureWarning
-warnings.filterwarnings("ignore", category=UserWarning)
-warnings.filterwarnings("ignore", category=FutureWarning)
-import yaml
-import torch
-import numpy as np
-import logging
-from fly_organelles.run import run
-from fly_organelles.model import StandardUnet
-
-from fly_organelles.run import set_weights
-import torch
-
+# Device setup
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 if torch.cuda.is_available():
     torch.backends.cuda.matmul.allow_tf32 = True
     torch.backends.cudnn.allow_tf32 = True
     torch.backends.cudnn.benchmark = True
 
-
 logger = logging.getLogger(__name__)
 
-
-#%%
-
-
-#%%
-
-
-
-iterations = 1000000
-
-else_map = {"isg":"ld",
-            "lyso":"organelle",}
-
-
+# Load datasets
 with open(yaml_file, "r") as data_yaml:
     datasets = yaml.safe_load(data_yaml)
-# label_stores, raw_stores, crop_copies = read_data_yaml(data_yaml)
 
+# Model setup
 model = StandardUnet(len(labels))
-#%%
-import glob
-import os
+
+# Checkpoint loading
 checkpoint_files = glob.glob(os.path.join(os.path.dirname(__file__), "model_checkpoint_*"))
 if checkpoint_files:
     def extract_step(fp):
@@ -81,19 +62,16 @@ else:
 
 model = model.to(device)
 
-
-run(model,
-iterations, 
-labels, 
-weights_labes, 
-datasets,
-voxel_size = voxel_size,
-batch_size = batch_size, 
-l_rate=l_rate,
-log_dir=log_dir,
-# affinities = True, 
-# affinities_map = affinities_map,
-# min_mask = 0.001,
-distance_sigma = 6,
+# Training
+run(
+    model,
+    iterations,
+    labels,
+    weights_labels,
+    datasets,
+    voxel_size=voxel_size,
+    batch_size=batch_size,
+    l_rate=l_rate,
+    log_dir=log_dir,
+    distance_sigma=6,
 )
-
